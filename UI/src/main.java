d093449b264f476271505e07ca07415c9462847a
EXTRAvaganza <37329819+EXTRAvaganza@users.noreply.github.com>
2020-02-19 18:02:21 +0400

    Select path DB

--- UI/src/main.java
+++ UI/src/main.java
@@ -1,7 +1,9 @@
+import javax.swing.*;
 import java.io.IOException;
 
 public class main {
-    public static void main(String[] args) throws IOException {
-        client kek = new client();
+    public static void main(String[] args) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        GUI gui =new GUI("Information System");
+        gui.setVisible(true);
     }
 }
